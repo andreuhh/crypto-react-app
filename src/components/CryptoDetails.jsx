4e28f21736a565b334f9eabb12709aa9fd8399dd
Andrea Guffi <andreaguffi92@gmail.com>
2021-12-29 16:19:30 +0100

    ok

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -24,23 +24,22 @@ const CryptoDetails = () => {
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
     const stats = [
-        { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
-        { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-        { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
-        { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
-        { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+        { num: 0, title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+        { num: 1, title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+        { num: 2, title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+        { num: 3, title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+        { num: 4, title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
     ];
 
     const genericStats = [
-        { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
-        { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-        { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-        { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
-        { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+        { num: 5, title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+        { num: 6, title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+        { num: 7, title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+        { num: 8, title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+        { num: 9, title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
     ];
 
     return (
-
         <Col className="coin-detail-container">
             <Col className="coin-heading-container">
                 <Title level={2} className="coin-name">
@@ -48,9 +47,9 @@ const CryptoDetails = () => {
                 </Title>
                 <p>{cryptoDetails.name} live price in US Dollar (USD). View value statistics, market cap and supply.</p>
             </Col>
-            <Select defaultValue="7d" className="select-timeperiod" placeholder="Select Timeperiod" onChange={(value) => setTimeperiod(value)}>
+            {/* <Select defaultValue="7d" className="select-timeperiod" placeholder="Select Timeperiod" onChange={(value) => setTimeperiod(value)}>
                 {time.map((date) => <Option key={date}>{date}</Option>)}
-            </Select>
+            </Select> */}
             <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} />
             <Col className="stats-container">
                 <Col className="coin-value-statistics">
@@ -58,14 +57,14 @@ const CryptoDetails = () => {
                         <Title level={3} className="coin-details-heading">{cryptoDetails.name} Value Statistics</Title>
                         <p>An overview showing the statistics of {cryptoDetails.name}, such as the base and quote currency, the rank, and trading volume.</p>
                     </Col>
-                    {stats.map(({ icon, title, value }) => (
-                        <Col key={title} className="coin-stats">
+                    {stats.map(({ num, icon, title, value }) => (
+                        <div key={num} className="coin-stats">
                             <Col className="coin-stats-name">
                                 <Text>{icon}</Text>
                                 <Text>{title}</Text>
                             </Col>
                             <Text className="stats">{value}</Text>
-                        </Col>
+                        </div>
                     ))}
                 </Col>
                 <Col className="other-stats-info">
@@ -73,14 +72,14 @@ const CryptoDetails = () => {
                         <Title level={3} className="coin-details-heading">Other Stats Info</Title>
                         <p>An overview showing the statistics of {cryptoDetails.name}, such as the base and quote currency, the rank, and trading volume.</p>
                     </Col>
-                    {genericStats.map(({ icon, title, value }) => (
-                        <Col key={title} className="coin-stats">
+                    {genericStats.map(({ num, icon, title, value }) => (
+                        <div key={num} className="coin-stats">
                             <Col className="coin-stats-name">
                                 <Text>{icon}</Text>
                                 <Text>{title}</Text>
                             </Col>
                             <Text className="stats">{value}</Text>
-                        </Col>
+                        </div>
                     ))}
                 </Col>
             </Col>
@@ -100,8 +99,6 @@ const CryptoDetails = () => {
                 </Col>
             </Col>
         </Col>
-
-
     );
 };
 
